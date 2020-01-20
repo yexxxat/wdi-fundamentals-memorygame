5ab716a822c9417c64adc753e03e06637465ee88
Yara Ezzat <y.exxat@gmail.com>
2020-01-21 00:44:56 +0300

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,16 @@
 console.log("Up and running!");
+/*
 let cardOne = "Queen";
 let cardTwo = "Queen";
 let cardThree = "King";
 let cardFour = "King";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+*/
+console.log("User flipped queen");
+console.log("User flipped king");
+const cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push('cardOne');
+var cardTwo = [2];
+cardsInPlay.push('secondCard');
+if (cardsInPlay.length === 2) { ; if (cardsInPlay[0] === cardsInPlay[1]) alert("You found a match!"); else alert("Sorry, try again"); }
