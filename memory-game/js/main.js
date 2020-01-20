21d6a5d808e17ce16f829f8e161a0db92a7096a7
Yara Ezzat <y.exxat@gmail.com>
2020-01-20 14:17:38 +0300

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+let cardOne = "Queen";
+let cardTwo = "Queen";
+let cardThree = "King";
+let cardFour = "King";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
